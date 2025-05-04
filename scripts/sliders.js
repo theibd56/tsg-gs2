dd03a93b0d3aa1af9c95a9ef02f9c3489ae9cc2f
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-05-04 15:56:05 +0700

    products sections has been created

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -1,1 +1,23 @@
-console.log('sliders')
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const productsSlider = new Swiper('.products .swiper', {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    speed: 600,
+    loop: false,
+    pagination: {
+        el: '.products .swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.products .prev',
+        nextEl: '.products .next'
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    }
+})
