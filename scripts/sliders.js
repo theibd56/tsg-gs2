3ede70c615f5e18e5fd412c4b3188ecef9f0faaa
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-05-04 16:01:23 +0700

    update script on slider

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -1,23 +1,25 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-const productsSlider = new Swiper('.products .swiper', {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    speed: 600,
-    loop: false,
-    pagination: {
-        el: '.products .swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        prevEl: '.products .prev',
-        nextEl: '.products .next'
-    },
-    breakpoints: {
-        992: {
-            slidesPerView: 3,
-            spaceBetween: 30,
+document.querySelectorAll('.products-slider').forEach(slider => {
+    new Swiper(slider.querySelector('.swiper'), {
+        slidesPerView: 3,
+        spaceBetween: 40,
+        speed: 600,
+        loop: false,
+        pagination: {
+            el: slider.querySelector('.swiper-pagination'),
+            clickable: true,
         },
-    }
-})
+        navigation: {
+            prevEl: slider.querySelector('.prev'),
+            nextEl: slider.querySelector('.next'),
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        }
+    });
+});
