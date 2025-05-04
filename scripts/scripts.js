311585ef27e54241432b75f4c108456830c97b21
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-05-04 15:00:59 +0700

    promo section has been created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,19 @@
-console.log('scripts')
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    const main = document.querySelector('main');
+
+    if (header && main) {
+        const headerHeight = header.offsetHeight;
+        main.style.paddingTop = `${headerHeight}px`;
+    }
+});
+
+window.addEventListener('resize', function() {
+    const header = document.querySelector('header');
+    const main = document.querySelector('main');
+
+    if (header && main) {
+        const headerHeight = header.offsetHeight;
+        main.style.paddingTop = `${headerHeight}px`;
+    }
+});
