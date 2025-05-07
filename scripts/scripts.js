fd1c72fa5ac2b6f6b714191faa3fa9adc6af7e86
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-05-07 21:41:25 +0700

    burger menu has been created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,12 @@
-import { Mask, MaskInput } from "maska";
-import { Fancybox } from "@fancyapps/ui";
+import {Mask, MaskInput} from "maska";
+import {Fancybox} from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 new MaskInput("[data-maska]")
 
 Fancybox.bind("[data-fancybox]", {});
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('header');
     const main = document.querySelector('main');
 
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     const header = document.querySelector('header');
     const main = document.querySelector('main');
 
@@ -24,4 +24,21 @@ window.addEventListener('resize', function() {
         const headerHeight = header.offsetHeight;
         main.style.paddingTop = `${headerHeight}px`;
     }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.header-burger');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const feedbackBtn = burgerMenu.querySelector('.footer-feedback');
+    const html = document.documentElement;
+
+    burger.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        html.classList.toggle('lock');
+    });
+
+    feedbackBtn.addEventListener('click', () => {
+        burgerMenu.classList.remove('active');
+        html.classList.remove('lock');
+    });
 });
